a52dc233b57328a68685310a2b1f74a79b184f7e
bobrenja <abcd43@ukr.net>
2023-01-24 19:48:23 +0200

    fix filter

--- src/components/PhoneBooks/MyPhoneBooksForm.jsx
+++ src/components/PhoneBooks/MyPhoneBooksForm.jsx
@@ -24,15 +24,18 @@ class MyPhoneBooksForm extends Component {
         number: '1234567',
       },
     ],
-    name: '',
-    number: '',
+
     filter: '',
   };
 
-  
+  handleEnterInput = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  };
 
-  addBook = ({name, number}) => {
-
+  addBook = ({ name, number }) => {
     this.setState(prevState => {
       const { contacts } = prevState;
       const newCont = { id: nanoid(), name: name, number: number };
@@ -43,25 +46,28 @@ class MyPhoneBooksForm extends Component {
 
   findContact() {
     const { filter, contacts } = this.state;
-if(!filter) {return contacts;}
-const normalaiseLow = filter.toLowerCase();
-const res = contacts.filter(({name, number}) => (name.toLowerCase().includes(normalaiseLow) || number.includes(normalaiseLow)) )
-// console.log(res)
-return res;
-
+    if (!filter) {
+      return contacts;
+    }
+    const normalaiseLow = filter.toLowerCase();
+    const res = contacts.filter(
+      ({ name, number }) =>
+        name.toLowerCase().includes(normalaiseLow) ||
+        number.includes(normalaiseLow)
+    );
+    // console.log(res)
+    return res;
   }
 
   render() {
-    const contactsFilter = this.findContact()
+    const contactsFilter = this.findContact();
     return (
       <>
-      
-       <AddContactForm  onSubmit={this.addBook}/>
-       
+        <AddContactForm onSubmit={this.addBook} />
+
         <label htmlFor="Search contact"></label>
         <input type="text" name="filter" onChange={this.handleEnterInput} />
-       
-       
+
         <div>Contact</div>
         <ol>
           {contactsFilter.map(({ id, name, number }) => (
